a3e1028d0b263e7f0a1d997dc69ce91cb7a42774
adrian-ferreira-jr <saitth.adrian@outlook.com>
2024-03-21 12:46:23 -0300

    button added icon

--- theses-repo/src/app/page.tsx
+++ theses-repo/src/app/page.tsx
@@ -1,10 +1,21 @@
 import { Button } from "@/components/button";
+import 'material-icons/iconfont/filled.css';
+import 'material-icons/iconfont/outlined.css';
 
 export default function Home() {
   return (
-    <main>
-      <div>
-        <Button>Button Test</Button>
+    <main className="dark:bg-dark-surface bg-surface">
+      <div className="inline-flex items-center justify-center whitespace-nowrap gap-8 p-8">
+        <Button iconPosition="left" icon="design_services">
+          Button Primary
+        </Button>
+        <Button variant="error" iconPosition="right" icon="delete_forever">
+          Button Error
+          </Button>
+        <Button variant="textDefault" iconPosition="left" icon="design_services">
+          Button Text Primary
+        </Button>
+        <Button variant="textError">Button Text Error</Button>
       </div>
     </main>
   );
--- theses-repo/src/components/button.tsx
+++ theses-repo/src/components/button.tsx
@@ -4,25 +4,24 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap transition-colors rounded-extra-small label-large ring-offset-white focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "py-2 inline-flex items-center justify-center whitespace-nowrap transition-colors rounded-extra-small label-large ring-offset-white focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
-        default: "bg-primary text-on-primary hover:bg-primary/90 dark:bg-dark-primary dark:text-dark-on-primary dark:hover:bg-dark-primary/90",
-        error:
-          "bg-red-500 text-slate-50 hover:bg-red-500/90 dark:bg-red-900 dark:text-slate-50 dark:hover:bg-red-900/90",
-        text: "hover:bg-slate-100 hover:text-slate-900 dark:hover:bg-slate-800 dark:hover:text-slate-50"
+        default: "bg-primary text-on-primary dark:bg-dark-primary dark:text-dark-on-primary hover:bg-opacity-90",
+        error:"bg-error text-on-error dark:bg-dark-error dark:text-dark-on-error hover:bg-opacity-90",
+        textDefault: "text-primary bg-primary bg-opacity-0 dark:text-dark-primary dark:bg-dark-primary dark:bg-opacity-0 hover:bg-opacity-20",
+        textError: "text-error bg-error bg-opacity-0 dark:text-dark-error dark:bg-dark-error dark:bg-opacity-0 hover:bg-opacity-20"
       },
-      size: {
-        default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-md px-3",
-        lg: "h-11 rounded-md px-8",
-        icon: "h-10 w-10",
-      },
+      iconPosition:{
+        left: "pl-3 pr-4 gap-2",
+        right: "pl-4 pr-3 gap-2",
+        none: "pl-4 pr-4"
+      }
     },
     defaultVariants: {
       variant: "default",
-      size: "default",
+      iconPosition: "none"
     },
   }
 )
@@ -31,17 +30,27 @@ export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean
+  icon?: string
+  iconPosition?: 'left' | 'right' | 'none' ;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  ({ className, variant, icon, iconPosition, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button"
     return (
       <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
+        className={cn(buttonVariants({ variant, iconPosition, className }))}
         ref={ref}
         {...props}
-      />
+      >
+        {icon && iconPosition === 'left' && (
+          <span className="material-icons">{icon}</span>
+        )}
+        {props.children}
+        {icon && iconPosition === 'right' && (
+          <span className="material-icons">{icon}</span>
+        )}
+      </Comp>
     )
   }
 )
