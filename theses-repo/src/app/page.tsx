e83c720a6b138c912adcfb4f3cce69ce2ef01610
adrian-ferreira-jr <saitth.adrian@outlook.com>
2024-03-21 11:14:18 -0300

    theme fix and button setup

--- theses-repo/src/app/page.tsx
+++ theses-repo/src/app/page.tsx
@@ -1,9 +1,10 @@
+import { Button } from "@/components/button";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between text-sm lg:flex">
-        <p className="label-small"> Label Small </p>
-        <p className="display-large"> Display Large </p>
+    <main>
+      <div>
+        <Button>Button Test</Button>
       </div>
     </main>
   );
